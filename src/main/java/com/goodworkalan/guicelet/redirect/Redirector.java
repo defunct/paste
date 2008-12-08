b527c17bd4d07f78d25892844a7cdf01a23fec3b
Alan Gutierrez <alan@blogometer.com>
2008-12-08 13:57:31 -0600

    - Redirector is now creating a Location header.

--- src/main/java/com/goodworkalan/guicelet/redirect/Redirector.java
+++ src/main/java/com/goodworkalan/guicelet/redirect/Redirector.java
@@ -1,10 +1,50 @@
 package com.goodworkalan.guicelet.redirect;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.goodworkalan.guicelet.Headers;
+
 
 public class Redirector
 {
+    private final HttpServletRequest request;
+    
+    private final Headers headers;
+    
+    public Redirector(HttpServletRequest request, Headers headers)
+    {
+        this.request = request;
+        this.headers = headers;
+    }
+    
     public void redirect(String where)
     {
+        redirect(where, 303);
+    }
+    
+    public void redirect(String where, int status)
+    {
+        URI uri = URI.create(request.getRequestURI());
+        URI location = URI.create(where.trim());
+        if (location.getScheme() == null)
+        {
+            String path = location.getPath();
+            if (path.length() != 0 && path.charAt(0) == '/')
+            {
+                path = request.getServletPath() + path;
+                headers.add("Location", uri.resolve(path).toString());
+            }
+            else
+            {
+                headers.add("Location", uri.resolve(path).toString());
+            }
+        }
+        else
+        {
+            headers.add("Location", location.toString());
+        }
     }
 
     public void parameter(String name, String value)
--- src/test/java/com/goodworkalan/guicelet/redirect/RedirectorTest.java
+++ src/test/java/com/goodworkalan/guicelet/redirect/RedirectorTest.java
@@ -1,12 +1,20 @@
 package com.goodworkalan.guicelet.redirect;
 
+import static org.mockito.Mockito.mock;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.testng.annotations.Test;
+
+import com.goodworkalan.guicelet.Headers;
 
 public class RedirectorTest
 {
     @Test
     public void constructor()  
     {
-        new Redirector();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Headers headers = new Headers("GET");
+        new Redirector(request, headers);
     }
 }
