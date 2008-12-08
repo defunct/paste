c5a656557d3a6dfdb5e8c99d6c745cdc71f83c51
Alan Gutierrez <alan@blogometer.com>
2008-12-08 13:41:33 -0600

    - Created a unit test for Redirector. - Added com.goodworkalan.guicelet.redirect to testng.xml.

---
+++ src/test/java/com/goodworkalan/guicelet/redirect/RedirectorTest.java
@@ -0,0 +1,12 @@
+package com.goodworkalan.guicelet.redirect;
+
+import org.testng.annotations.Test;
+
+public class RedirectorTest
+{
+    @Test
+    public void constructor()  
+    {
+        new Redirector();
+    }
+}
