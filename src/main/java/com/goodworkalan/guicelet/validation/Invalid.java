01cc4a28a864f3a8ecbf6bb58cd0b898f8a9484f
Alan Gutierrez <alan@blogometer.com>
2008-12-14 19:53:13 -0600

    - Created invalid exception which will report validation errors.

---
+++ src/main/java/com/goodworkalan/guicelet/validation/Invalid.java
@@ -0,0 +1,6 @@
+package com.goodworkalan.guicelet.validation;
+
+public class Invalid extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/test/java/com/goodworkalan/guicelet/validation/InvalidTest.java
@@ -0,0 +1,12 @@
+package com.goodworkalan.guicelet.validation;
+
+import org.testng.annotations.Test;
+
+public class InvalidTest
+{
+    @Test
+    public void constructor()
+    {
+        new Invalid();
+    }
+}
