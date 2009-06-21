51bef165d233f5f7c80bdf6dac77a0e5e027a285
Alan Gutierrez <alan@blogometer.com>
2009-06-21 16:47:21 -0500

    - Javadoc for the Parameters class.

--- src/main/java/com/goodworkalan/paste/util/Parameters.java
+++ src/main/java/com/goodworkalan/paste/util/Parameters.java
@@ -4,14 +4,33 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 
-
+/**
+ * A list of name value pairs interpreted as a list of request parameters.
+ * This class adds a convenience method to interpret the list as a query
+ * string. The type exists for the the sake of Guice bindings, to give
+ * a more meaningful type name to the request parameters object.
+ *     
+ * @author Alan Gutierrez
+ */
 public class Parameters extends NamedValueList
 {
+    /**
+     * Create a list of parameters as wrapper around the given list of named
+     * values.
+     * 
+     * @param namedValues
+     *            The list of name values to copy.
+     */
     public Parameters(List<NamedValue> namedValues)
     {
         super(namedValues);
     }
-    
+
+    /**
+     * Create a URL encoded query string from the list of named values.
+     * 
+     * @return A URL encoded query string.
+     */
     public String toQueryString()
     {
         StringBuilder queryString = new StringBuilder();
