05d7bcb4c72aee03906aae960e63572d22b12a93
Alan Gutierrez <alan@blogometer.com>
2009-06-22 09:31:22 -0500

    - Javadoc for Evaluator.

--- src/main/java/com/goodworkalan/paste/Evaluator.java
+++ src/main/java/com/goodworkalan/paste/Evaluator.java
@@ -3,24 +3,41 @@ package com.goodworkalan.paste;
 import com.goodworkalan.infuse.Diffusion;
 import com.goodworkalan.infuse.PathException;
 
-// TODO Document.
+/**
+ * Extract values from an object using diffusion paths.
+ * 
+ * @author Alan Gutierrez
+ */
 public class Evaluator
 {
-    // TODO Document.
-    private final Object controller;
-    
-    // TODO Document.
-    public Evaluator(Object controller)
+    /** The object to diffuse. */
+    private final Object object;
+
+    /**
+     * Create a new evaluator that extracts values from the given object.
+     * 
+     * @param object
+     *            The object to diffuse.
+     */
+    public Evaluator(Object object)
     {
-        this.controller = controller;
+        this.object = object;
     }
 
-    // TODO Document.
+    /**
+     * Extract a value from the object property using the given object path
+     * expression.
+     * 
+     * @param expression
+     *            The object path expression.
+     * @return The object value as a string or null if any part of the path does
+     *         not exist.
+     */
     public String get(String expression)
     {
         try
         {
-            Diffusion diffusion = new Diffusion(controller);
+            Diffusion diffusion = new Diffusion(object);
             Object object = diffusion.get(expression);
             return object == null ? null : object.toString();
         }
