d3135b5b9809ca651ae0ca6e1612dd5200b6245f
Alan Gutierrez <alan@blogometer.com>
2009-08-19 13:23:56 -0500

    Javadoc for InitializationParameters annotation.

--- src/main/java/com/goodworkalan/paste/InitializationParameters.java
+++ src/main/java/com/goodworkalan/paste/InitializationParameters.java
@@ -1,5 +1,6 @@
 package com.goodworkalan.paste;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,10 +8,15 @@ import java.lang.annotation.Target;
 
 import com.google.inject.BindingAnnotation;
 
-// TODO Document.
+/**
+ * Indicates the map of strings to strings that contains the initialization
+ * parameters passed to the Paste filter.
+ * 
+ * @author Alan Gutierrez
+ */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.PARAMETER })
 @BindingAnnotation
-public @interface InitializationParameters
-{
+public @interface InitializationParameters {
 }
