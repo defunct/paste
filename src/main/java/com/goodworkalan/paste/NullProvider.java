a667127e324ba8ed4d071b3148d9026522ee63bf
Alan Gutierrez <alan@blogometer.com>
2009-08-19 19:54:29 -0500

    More Javadoc for NullProvider.

--- src/main/java/com/goodworkalan/paste/NullProvider.java
+++ src/main/java/com/goodworkalan/paste/NullProvider.java
@@ -3,11 +3,16 @@ package com.goodworkalan.paste;
 import com.google.inject.Provider;
 
 /**
- * A dummy provider for properties that are explicity set into the scope.
+ * A dummy provider for properties that are explicitly set into the scope.
+ * <p>
+ * Note that in the Google Guice scopes examples, they use a singleton class
+ * that implements <code>Provider&lt;Object%gt;</code>. We can't do that because
+ * it does not work with parameterized values for <code>T</code>.
  * 
  * @author Alan Gutierrez
  * 
- * @param <T> The type to provide.
+ * @param <T>
+ *            The type to provide.
  */
 public class NullProvider<T> implements Provider<T> {
     /**
