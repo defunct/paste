eee9329caeeff4e8b7b77efcf1a00ef521ab7390
Alan Gutierrez <alan@blogometer.com>
2009-12-31 16:33:46 -0600

    Render exception to jump to render, version bump.

---
+++ src/main/java/com/goodworkalan/paste/Render.java
@@ -0,0 +1,6 @@
+package com.goodworkalan.paste;
+
+public class Render extends RuntimeException {
+    /** The serial version id. */
+    private static final long serialVersionUID = 1L;
+}
--- src/mix/java/com/goodworkalan/paste/mix/PasteProject.java
+++ src/mix/java/com/goodworkalan/paste/mix/PasteProject.java
@@ -10,7 +10,7 @@ public class PasteProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/paste/0.2"))
+                .produces(new Artifact("com.goodworkalan/paste/0.2.1"))
                 .main()
                     .depends()
                         .artifact(new Artifact("org.slf4j/slf4j-api/1.4.2"))
