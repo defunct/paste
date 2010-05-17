5c1aa3388e1c024d37fdf2af5f8a93a85699e4bc
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:15:34 -0500

    Fixed Mix, removed old Infuse.
     * Removed old infuse.
     * Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/paste/mix/PasteProject.java
+++ src/mix/java/com/goodworkalan/paste/mix/PasteProject.java
@@ -4,38 +4,42 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class PasteProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Paste.
+ *
+ * @author Alan Gutierrez
+ */
+public class PasteProject implements ProjectModule {
+    /**
+     * Build the project definition for Paste.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.paste/paste/0.2.1")
-                .main()
-                    .depends()
-                        .include("org.slf4j/slf4j-api/1.4.2")
-                        .include("com.github.bigeasy.string-beans/string-beans-url/0.+1")
-                        .include("com.github.bigeasy.infuse/infuse/0.+1")
-                        .include("com.github.bigeasy.dovetail/dovetail/0.+7/")
-                        .include("aopalliance/aopalliance/1.0")
-                        .include("com.google.inject/guice/2.0")
-                        .include("com.google.inject/guice-servlet/2.0")
-                        .include("com.github.bigeasy.deviate/deviate/0.+1")
-                        .include("org.hibernate/hibernate-core/3.3.1.GA")
-                        .include("org.hibernate/hibernate-annotations/3.4.0.GA")
-                        .include("com.mallardsoft/tuple/2.0")
-                        .include("javax.servlet/servlet-api/2.5")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .include("org.eclipse.jetty/jetty-xml/7.0.0.RC3")
-                        .include("org.eclipse.jetty/jetty-webapp/7.0.0.RC3")
-                        .include("org.eclipse.jetty/jetty-plus/7.0.0.RC3")
-                        .include("org.eclipse.jetty/jetty-jndi/7.0.0.RC3")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("org.slf4j/slf4j-api/1.4.2")
+                    .production("com.github.bigeasy.string-beans/string-beans-url/0.+1")
+                    .production("com.github.bigeasy.infuse/infuse/0.+1")
+                    .production("com.github.bigeasy.dovetail/dovetail/0.+7/")
+                    .production("aopalliance/aopalliance/1.0")
+                    .production("com.google.inject/guice/2.0")
+                    .production("com.google.inject/guice-servlet/2.0")
+                    .production("com.github.bigeasy.deviate/deviate/0.+1")
+                    .production("org.hibernate/hibernate-core/3.3.1.GA")
+                    .production("org.hibernate/hibernate-annotations/3.4.0.GA")
+                    .production("com.mallardsoft/tuple/2.0")
+                    .production("javax.servlet/servlet-api/2.5")
+                    .development("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("org.eclipse.jetty/jetty-xml/7.0.0.RC3")
+                    .development("org.eclipse.jetty/jetty-webapp/7.0.0.RC3")
+                    .development("org.eclipse.jetty/jetty-plus/7.0.0.RC3")
+                    .development("org.eclipse.jetty/jetty-jndi/7.0.0.RC3")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
